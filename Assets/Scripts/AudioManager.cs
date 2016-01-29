5ca16d16c5d091a74491db9acdfb3e91eb6fae84
Fernando Urquio <furquijos@gmail.com>
2016-01-29 23:20:54 +0100

    Commit base proyecto

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager audioManagerInstance = null;   
+	public AudioClip[] clips;
+	public AudioSource audioSource;
+
+	void Awake()
+	{
+		//Check if instance already exists
+		if (audioManagerInstance == null){
+			audioManagerInstance = this;
+		}
+
+		//If instance already exists and it's not this:
+		else if (audioManagerInstance != this){
+			Destroy(gameObject);    
+		}
+		DontDestroyOnLoad(gameObject);
+
+	}
+	void Start(){
+		audioSource = GetComponent<AudioSource> () as AudioSource;
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;   
+	private int age = 0;  
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+			//Check if instance already exists
+		if (instance == null){
+			instance = this;
+		}
+
+			//If instance already exists and it's not this:
+		else if (instance != this){
+			Destroy(gameObject);    
+		}
+			DontDestroyOnLoad(gameObject);
+			InitGame();
+	}
+
+	void InitGame(){
+
+
+	}
+
+	void Update()
+	{
+
+	}
+}

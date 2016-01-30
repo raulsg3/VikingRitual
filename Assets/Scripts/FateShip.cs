5dc247bfd04a19800ceea0f10de14777fdb46034
raulsg3 <raulseguragaleote@gmail.com>
2016-01-30 18:45:43 +0100

    FateScene base done

--- Assets/Scripts/FateShip.cs
+++ Assets/Scripts/FateShip.cs
@@ -8,10 +8,18 @@ public class FateShip : MonoBehaviour {
 
     // Shooting parameters
     public float timeBetweenShots = 0.5f;
-    public float initialVelocity = 50f;
+    public float projectileSpeed = 30f;
 
     private float timeSinceLastShot = 0;
+    private bool shooting = false;
 
+    // Rotating
+    public float turnSpeed = 10.0f;
+
+    private bool rotating = false;
+    private Vector3 hitPoint;
+    private Quaternion lookRotation;
+    
     // Projectile
     public Rigidbody projectile;
 
@@ -21,12 +29,49 @@ public class FateShip : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         timeSinceLastShot += Time.deltaTime;
 
-        if (canShoot()) {
-            shoot();
-            timeSinceLastShot = 0.0f;
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = (Camera.main.ScreenPointToRay(Input.mousePosition));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                //if (hit.transform.tag == "Ground") {
+                hitPoint = hit.point;
+                rotating = true;
+                shooting = true;
+                //}
+            }
+
+        }
+
+        //calculate the displacement since last frame:
+        //find the vector pointing from our position to the target
+        if (hitPoint != null)
+        {
+            Vector3 _direction = Vector3.Normalize(hitPoint - transform.position);
+
+            if (rotating)
+            {
+                //rotate us over time according to speed until we are in the required rotation
+                lookRotation = Quaternion.LookRotation(_direction);
+                //create the rotation we need to be in to look at the target
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+            }
+
+            if (Quaternion.Angle(Quaternion.LookRotation(transform.forward), lookRotation) < 10)
+            {
+                rotating = false;
+
+                if (shooting && canShoot())
+                {
+                    shoot();
+                    timeSinceLastShot = 0.0f;
+                    shooting = false;
+                }
+            }
         }
 
     }
@@ -47,7 +92,7 @@ public class FateShip : MonoBehaviour {
         newProjectile.transform.position = shootPoint.position;
         newProjectile.transform.forward = shootPoint.forward;
 
-        newProjectile.velocity = newProjectile.transform.forward * initialVelocity;
+        newProjectile.velocity = newProjectile.transform.forward * projectileSpeed;
 
         Collider shipCollider = transform.root.GetComponent<Collider>();
         Collider newProjectileCollider = newProjectile.gameObject.GetComponent<Collider>();

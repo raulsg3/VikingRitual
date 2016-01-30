fe4699435036970c52bca90a497302593ed0f9ce
Fernando Urquio <furquijos@gmail.com>
2016-01-30 11:27:59 +0100

    Proyecto Base con manager y botonos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;   
+	public enum Scenes{MainScene, FertilityScene, RainScene, ProvidenceScene, FadeScene}
+	Scenes currentScene;
+	public Slider rainSlider;
+	public Slider providenceSlider;
+	public Slider fertilitySlider;
+	public Slider fadeSlider;
+
+
+
 	[SerializeField]
 	int age = 0;
 	float fertilityValue = 10f;
@@ -28,13 +40,45 @@ public class GameManager : MonoBehaviour {
 			InitGame();
 	}
 
+	void Start(){
+	}
+
 	void InitGame(){
 
 
 	}
 
-	void Update()
-	{
+
+	public void Loadscene(string SceneName){
+		
+		SceneManager.LoadScene(SceneName);
+	}
+
+	public void LoadMainScene(){
+		SceneManager.LoadScene("MainScene");
+	}
+
+	public void SetAttributeValue(float value, Scenes newScene){
+		switch(newScene)
+		{
+		case Scenes.FertilityScene:
+			fertilityValue = value;
+			fertilitySlider.value = value;
+			break;
+		case Scenes.FadeScene:
+			fadeValue = value;
+			fadeSlider.value = value;
+			break;
+		case Scenes.ProvidenceScene:
+			providenceValue = value;
+			providenceSlider.value = value;
+			break;
+		case Scenes.RainScene:
+			rainValue = value;
+			rainSlider.value = value;
+			break;
+		}
 
 	}
+		
 }

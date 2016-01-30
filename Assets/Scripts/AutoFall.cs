7113cdacd761f588f858a4b628529169a2bdd734
raulsg3 <raulseguragaleote@gmail.com>
2016-01-30 21:43:42 +0100

    Providence scene complete but raycasting

---
+++ Assets/Scripts/AutoFall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoFall : MonoBehaviour {
+    
+    public float timeUntilFall = 2.0f;
+    
+    void Start() {
+        StartCoroutine(Fall());
+    }
+
+    IEnumerator Fall()
+    {
+        // Waiting...
+        yield return new WaitForSeconds(timeUntilFall);
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity *= -1; 
+    }
+}

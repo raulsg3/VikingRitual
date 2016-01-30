d7b012327c7207d08bb4568ebae542969c5a287a
alejandro carballo <acarball@bitbucket.org>
2016-01-30 19:32:41 +0100

    Cambiada el nombre de al escena a nombre final RainScene

---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    #region Variables del player
+    public Vector3 _Motion;
+    public float _gravity = 0.1f;
+    public bool bFalling = false;
+    private Rigidbody rBody;
+
+    private Vector3 vVectorGround;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        _Motion = new Vector3(0, 0, 0);
+        rBody = gameObject.GetComponent<Rigidbody>();
+
+        vVectorGround = new Vector3(0.001f, 0.001f, 0.001f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimientoç
+
+
+    }
+    /// <summary>
+    /// gestor del salto del personaje
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator iJump(float force)
+    {
+        Debug.Log("invocando el salto con: " + rBody.velocity.y + " - " + bFalling);
+        if (/*rBody.velocity.y <= 0.1f &&*/ !bFalling)
+        {
+            bFalling = true;
+            _Motion.y = (force) * _gravity;
+            rBody.velocity = _Motion;
+            Debug.Log("Saltando!?  " + rBody.velocity.y);
+            yield return 0;
+        }
+    }
+
+    /// <summary>
+    /// Gestionamos el fin del minijuego.
+    /// </summary>
+    private void OnDeath()
+    {
+
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="colision"></param>
+    void OnTriggerEnter(Collider colision)
+    {
+        if (colision.gameObject.tag == "obstacle")
+        {
+
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="colision"></param>
+    void OnCollisionEnter(Collision colision)
+    {
+        Debug.Log("dentro de ontrgStay :" + colision.gameObject.tag);
+        if (colision.gameObject.tag == "Ground")
+        {
+            bFalling = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/RainManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainManager : MonoBehaviour
+{
+    #region variables configurables de la escena
+    public float fFloorSpeed = 0.0f;
+    public float fJumpHeight = 2.0f;
+    public float fTuneSpawnObj = 0.0f;
+    public GameObject goPlayer;
+    public GameObject goMoebiusStrip;
+    #endregion
+
+    private bool bStartAnim;
+    private bool bJummping;
+    // Use this for initialization
+    void Start()
+    {
+        bStartAnim = false;
+        bJummping = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        bJummping = goPlayer.GetComponent<Playercontroller>().bFalling;
+        if (Input.GetMouseButtonDown(0) && !bJummping)
+        {
+            //Comprobamos que unicamente se lanze la animación de inicio cuando corresponda
+            if (!bStartAnim)
+                goMoebiusStrip.GetComponent<Animator>().SetTrigger("StartTrigger");
+
+            bJummping = true;
+            StartCoroutine(goPlayer.GetComponent<Playercontroller>().iJump(fJumpHeight));
+
+        }
+
+    }
+
+    /// <summary>
+    /// Controlamos la velocidad del suelo y cuando debe parar
+    /// </summary>
+    private void FloorSpeed()
+    {
+
+    }
+
+
+}

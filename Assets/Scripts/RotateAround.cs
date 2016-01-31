05a6ae234bd29b4e5f877ab2b5028ae90ceaeaa8
Fernando Urquio <furquijos@gmail.com>
2016-01-31 13:19:13 +0100

    añadiendo botones

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+	public float speed = 10f;
+
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+	}
+}
